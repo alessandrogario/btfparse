5825656630bfcae6623ae3e41346573e30beffc4
Alessandro Gario <alessandro.gario@gmail.com>
2023-12-25 15:48:24 +0100

    ci: Add initial CI support

---
+++ .github/workflows/scripts/install_rust.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+main() {
+  which curl > /dev/null 2>&1
+  if [[ $? != 0 ]] ; then
+    printf "The 'curl' command is not available. Please install it and try again.\n"
+    return 1
+  fi
+
+  curl \
+    --proto '=https' \
+    --tlsv1.2 \
+    -sSf \
+    https://sh.rustup.rs > "rustup.sh"
+
+  chmod +x "rustup.sh"
+
+  ./rustup.sh -y --quiet
+  if [[ $? != 0 ]] ; then
+    printf "Failed to install Rust.\n"
+    return 1
+  fi
+
+  return 0
+}
+
+main $@
+exit $?
