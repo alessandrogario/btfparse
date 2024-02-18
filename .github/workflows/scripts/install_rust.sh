ebd889afa63d9114e50f1e23d12bde905cb7ca13
Alessandro Gario <alessandro.gario@gmail.com>
2024-02-18 21:03:19 +0100

    docs: Add copyright headers

--- .github/workflows/scripts/install_rust.sh
+++ .github/workflows/scripts/install_rust.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+#
+# Copyright (c) 2024-present, Alessandro Gario
+# All rights reserved.
+#
+# This source code is licensed in accordance with the terms specified in
+# the LICENSE file found in the root directory of this source tree.
+#
+
 main() {
   which curl > /dev/null 2>&1
   if [[ $? != 0 ]] ; then
--- btfparse/src/btf/type_information.rs
+++ btfparse/src/btf/type_information.rs
@@ -1,3 +1,11 @@
+/*
+  Copyright (c) 2024-present, Alessandro Gario
+  All rights reserved.
+
+  This source code is licensed in accordance with the terms specified in
+  the LICENSE file found in the root directory of this source tree.
+*/
+
 use crate::btf::{
     Array, Const, DataSec, DeclTag, Enum, Enum64, Error as BTFError, ErrorKind as BTFErrorKind,
     FileHeader, Float, Func, FuncProto, Fwd, Header, Int, Kind, Offset, Ptr, Readable, Restrict,
